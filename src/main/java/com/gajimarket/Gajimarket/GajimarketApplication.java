806202a60a8d8af4b1c320c725bf1c874299e529
Soyoung Lee <soo7132@naver.com>
2024-11-05 23:57:12 +0900

    spring project 추가

---
+++ src/main/java/com/gajimarket/Gajimarket/GajimarketApplication.java
@@ -0,0 +1,13 @@
+package com.gajimarket.Gajimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GajimarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GajimarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gajimarket/Gajimarket/GajimarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gajimarket.Gajimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GajimarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
